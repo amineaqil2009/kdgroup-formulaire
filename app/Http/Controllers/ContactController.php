56e8d573ce8944e19db8ec9e031ca5ceb7c1c90e
= <=>
2019-10-15 22:05:31 +0100

    create repository

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests\ContactRequest;
 use App\Message;
-
+use App\Repositories\ContactRepository;
 use Mail;
 
 class ContactController extends Controller
 {
-    //
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository)
+    {
+        $this->contactRepository = $contactRepository;
+    }
 
     public function create(){
     	return  view('contact');
@@ -21,19 +24,15 @@ class ContactController extends Controller
 
     public function store(ContactRequest $request)
     {
-
-    	$message = new Message;
-
-    	$message->sujet = $request->sujet;
-		$message->email = $request->email;
-    	$message->description = $request->description;
-    	$message->save();
+    	$message = $this->contactRepository->store(
+            $request->except('_token', 'q')
+        );
 
     	Mail::send('email.message',[
-    		'msg' => $request->description
-    	], function ($mail) use($request){
-    		$mail->from($request->email);
-    		$mail->to('amine.aqil2009@gmail.com')->subject($request->sujet);
+    		'msg' => $message->description
+    	], function ($mail) use($message){
+    		$mail->from($message->email);
+    		$mail->to('amine.aqil2009@gmail.com')->subject($message->sujet);
     	});
 
     	return redirect()->back()->with('flash_message', 'Votre message a bien été envoyé');
